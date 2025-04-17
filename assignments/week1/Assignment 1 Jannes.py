376ee57f5b69d2800aff6da752d0373e2b7794b3
schmidt <schmidt.jannes04@gmail.com>
2025-04-17 20:26:16 +0200

    improved version for assignment 1

--- assignments/week1/Assignment 1 Jannes.py
+++ assignments/week1/Assignment 1 Jannes.py
@@ -1,69 +1,7 @@
-{
- "cells": [
-  {
-   "metadata": {},
-   "cell_type": "markdown",
-   "source": [
-    "Excercise 1:\n",
-    "Make a little piece of ASCII art with print functions in python. You can first try it out here in the notebook. After you are satisfied with your result, save it as a .py file"
-   ],
-   "id": "3a402535f85bfb94"
-  },
-  {
-   "metadata": {
-    "ExecuteTime": {
-     "end_time": "2025-04-10T20:37:14.348359Z",
-     "start_time": "2025-04-10T20:37:14.344562Z"
-    }
-   },
-   "cell_type": "code",
-   "source": [
-    "print('Boy shows his father stinky fish for Xmas:')\n",
-    "print('            *                          _W_         *   /   \\\\   *      ')\n",
-    "print('  *   _~_ *       *         *      *  (O_O) *   *     /     \\\\  ' )\n",
-    "print('     ( ^-^)            *            /(   o   )\\\\      /_______\\\\   *   ')\n",
-    "print('*  /(   x   )\\\\  *      ( ( (  *    /  | | | |  \\\\  *  |       |    *  ')\n",
-    "print('  o  | | | |  o         ) ) )     O   | | | |   O    |  | |  |     ')\n",
-    "print(\"     '_' '_'        *  >+++°>         |_| |_|        |_______|   *        \")"
-   ],
-   "id": "5cf9f71a4391fcf",
-   "outputs": [
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      "Boy shows his father stinky fish for Xmas:\n",
-      "            *                          _W_         *   /   \\   *      \n",
-      "  *   _~_ *       *         *      *  (O_O) *   *     /     \\  \n",
-      "     ( ^-^)            *            /(   o   )\\      /_______\\   *   \n",
-      "*  /(   x   )\\  *      ( ( (  *    /  | | | |  \\  *  |       |    *  \n",
-      "  o  | | | |  o         ) ) )     O   | | | |   O    |  | |  |     \n",
-      "     '_' '_'        *  >+++°>         |_| |_|        |_______|   *        \n"
-     ]
-    }
-   ],
-   "execution_count": 1
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 2
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython2",
-   "version": "2.7.6"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+print('Boy shows his father stinky fish for Xmas:')
+print('            *                          _W_         *   /   \\  *      ')
+print('  *   _~_ *       *         *      *  (O_O) *   *     /     \\ ' )
+print('     ( ^-^)            *            /(   o   )\\\\     /_______\\   *   ')
+print('*  /(   x   )\\\\  *     ( ( (  *    /  | | | |  \\\\ *  |       |    *  ')
+print('  o  | | | |  o         ) ) )     O   | | | |   O    |  | |  |     ')
+print("     '_' '_'        *  >+++°>         |_| |_|        |_______|   *        ")
